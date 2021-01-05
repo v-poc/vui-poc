e71967e41f39cc90122231b090bd7d5526483291
Niko <bluepspower@163.com>
2021-01-06 01:58:42 +0800

    feat: use vite2

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  base: '/vui-poc'
-}
+import { defineConfig } from 'vite'
+import vuePlugin from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [ vuePlugin() ],
+  build: {
+    base: '/vui-poc/'
+  }
+})
